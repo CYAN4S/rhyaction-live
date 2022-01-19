2299c402ff05c62258de75553658773bcc38033c
CYAN4S <me@cyan4s.com>
2022-01-19 23:37:26 +0900

    Add basic input handling

--- Assets/NoteSystem.cs
+++ Assets/NoteSystem.cs
@@ -9,33 +9,36 @@ namespace CYAN4S
     public class NoteSystem : MonoBehaviour
     {
         private static float[] _xPos;
-        private static Func<float> _yPos; // TODO: Use this.
+        private static Func<Fraction, float> _yPos;
 
         private RectTransform _rectTransform;
         private NoteData _noteData;
+        
+        public float Time { get; private set; }
 
         private void Awake()
         {
             _rectTransform = GetComponent<RectTransform>();
         }
 
-        public static void StaticInitialize(float[] xPos, Func<float> yPos)
+        public static void StaticInitialize(float[] xPos, Func<Fraction, float> yPos)
         {
             _xPos = xPos;
             _yPos = yPos;
         }
 
-        public void InstanceInitialize(NoteData noteData)
+        public void InstanceInitialize(NoteData noteData, float time)
         {
             _noteData = noteData;
-
-            _rectTransform.localPosition = new Vector3(_xPos[_noteData.line], (float) _noteData.beat * 100);
+            Time = time;
+            _rectTransform.localPosition = new Vector3(_xPos[_noteData.line], 0f);
         }
 
         private void Update()
         {
             _rectTransform.localPosition =
-                new Vector3(_xPos[_noteData.line], (float) (_noteData.beat - Player.CurrentBeat) * 1000f);
+                // new Vector3(_xPos[_noteData.line], (float) (_noteData.beat - Player.CurrentBeat) * 1000f);
+                new Vector3(_xPos[_noteData.line], _yPos(_noteData.beat));
         }
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -11,27 +11,43 @@ namespace CYAN4S
     {
         [SerializeField] private NoteSystem notePrefab;
         [SerializeField] private RectTransform notesParent;
-        
+
         [SerializeField] private float[] xPos;
+        [SerializeField] private float[] judgeStandard;
+        [SerializeField] [Range(1.0f, 9.9f)] private float scrollSpeed;
 
         public List<NoteData> notes = new List<NoteData>();
+
         public static float CurrentTime { get; private set; }
         public static double CurrentBeat { get; private set; }
 
+        private List<Queue<NoteSystem>> _noteQueue = new List<Queue<NoteSystem>>();
+
+
         private void Awake()
         {
-            // TODO
-            for (int i = 0; i < 100; i++)
+            // TODO DATA
+            for (int i = 0; i < 1000; i++)
             {
                 notes.Add(new NoteData(new Fraction(i, 4), i % 4, null));
             }
 
-            NoteSystem.StaticInitialize(xPos, () => 255f);
+            // TODO DATA
+            for (int i = 0; i < 4; i++)
+            {
+                _noteQueue.Add(new Queue<NoteSystem>());
+            }
+
+            // TODO MATH
+            NoteSystem.StaticInitialize(xPos, beat => (float) (beat - CurrentBeat) * 100f * scrollSpeed);
 
             foreach (var note in notes)
             {
                 var noteSystem = Instantiate(notePrefab, notesParent);
-                noteSystem.InstanceInitialize(note);
+                // TODO MATH
+                noteSystem.InstanceInitialize(note, (float)note.beat * 0.5f);
+                
+                _noteQueue[note.line].Enqueue(noteSystem);
             }
 
             // Value Initialize
@@ -42,6 +58,23 @@ namespace CYAN4S
         {
             CurrentTime += Time.deltaTime;
             CurrentBeat = CurrentTime / 60d * 120d;
+
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+            {
+                _noteQueue[0].Dequeue().gameObject.SetActive(false);
+            }
+            if (Keyboard.current.rKey.wasPressedThisFrame)
+            {
+                _noteQueue[1].Dequeue().gameObject.SetActive(false);
+            }
+            if (Keyboard.current.pKey.wasPressedThisFrame)
+            {
+                _noteQueue[2].Dequeue().gameObject.SetActive(false);
+            }
+            if (Keyboard.current.leftBracketKey.wasPressedThisFrame)
+            {
+                _noteQueue[3].Dequeue().gameObject.SetActive(false);
+            }
         }
     }
 }
